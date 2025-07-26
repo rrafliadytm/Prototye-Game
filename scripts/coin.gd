284bf1ab8d25fe5e4febf668c97347fbfdbacfb3
Muhammad Rafli Adyatma <rafliadyatma17@gmail.com>
2025-07-26 16:26:07 +0700

    add sfx coin

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,9 +1,9 @@
 extends Area2D
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-@onready var audio_stream_player_2d: AudioStreamPlayer2D = $CollisionShape2D/AudioStreamPlayer2D
+
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_score()
-	queue_free()
+	animation_player.play("pickup_coin")
 	
