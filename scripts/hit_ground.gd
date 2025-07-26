7228deec518012e1eed6d7892b971ac212796012
Muhammad Rafli Adyatma <rafliadyatma17@gmail.com>
2025-07-26 16:45:50 +0700

    add falling damage sfx

--- scripts/hit_ground.gd
+++ scripts/hit_ground.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _on_body_entered(body):
 	Engine.time_scale = 0.9
+	audio_stream_player_2d.playing =true
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
