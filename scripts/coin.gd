a3096a96f9cb4df68041371283c5bb97f0a4cb6a
Muhammad Rafli Adyatma <rafliadyatma17@gmail.com>
2025-07-26 15:56:15 +0700

    add score counter

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,9 @@
 extends Area2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $CollisionShape2D/AudioStreamPlayer2D
+@onready var game_manager: Node = %GameManager
 
 func _on_body_entered(body: Node2D) -> void:
-	print("+1 Coin")
+	game_manager.add_score()
 	queue_free()
 	
